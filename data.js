99a3712e9bcef0bc34f0aafafa860b34a0a1d058
Toshi <dtkellogg10@gmail.com>
2023-03-27 17:21:34 -0400

    Data - sample data for Tarija and Sucre written

---
+++ data.js
@@ -0,0 +1,38 @@
+const locations = [
+  {
+    name: 'Tarija',
+    county: 'Bolivia',
+    summary: '',
+    startDate: '',
+    endDate: '',
+    highlights: [
+      {
+        name: 'Carnival',
+        date: '',
+        photos: [
+          '#',
+          '#'
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Sucre',
+    county: 'Bolivia',
+    summary: '',
+    startDate: '',
+    endDate: '',
+    highlights: [
+      {
+        name: 'Carnival',
+        date: '',
+        photos: [
+          '#',
+          '#'
+        ]
+      }
+    ]
+  }
+]
+
+export { locations }
