fb36d8162a38739a22423647e69012180ecd1121
Toshi <dtkellogg10@gmail.com>
2023-03-27 17:41:43 -0400

    Dependencies and Routing - installed necessary dependencies. Set up routing with 1 route (Locaiton)

---
+++ screens/LocationScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function LocationScreen() {
+  return (
+    <View>
+      <Text className="text-green-500">Location Screenn</Text>
+    </View>
+  )
+}
