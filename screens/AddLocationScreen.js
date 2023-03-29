57d5b5c7d9239484bcd91e77d62b2c386dd61505
Toshi <dtkellogg10@gmail.com>
2023-03-29 16:46:56 -0400

    Add Location Screen - fields added for all necessary fields

--- screens/AddLocationScreen.js
+++ screens/AddLocationScreen.js
@@ -1,9 +1,16 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import { View, Text, ScrollView, Image, TouchableOpacity, TextInput, StyleSheet, Button } from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { ArrowLeftIcon } from 'react-native-heroicons/solid'
 
 export default function AddLocationScreen() {
+  const [name, changeName] = useState()
+  const [country, changeCountry] = useState()
+  const [img, changeImg] = useState()
+  const [summary, changeSummary] = useState()
+  const [startDate, changeStartDate] = useState()
+  const [endDate, changeEndDate] = useState()
+
   const navigation = useNavigation()
 
   useLayoutEffect(() => {
@@ -12,20 +19,84 @@ export default function AddLocationScreen() {
     })
   })
 
+  const onSubmit = () => {
+    console.log('Add Location Form Submitted!')
+  }
+
   return (
-    <ScrollView>
-      <Text>Add Location</Text>
+    <ScrollView className="pt-10">
+      <Text className="text-3xl text-center">Add Location</Text>
       {/* Back btn */}
-      <View className="relative">
+      {/* <View className="relative"> */}
         <TouchableOpacity
           onPress={navigation.goBack}
-          className="absolute top-10 left-5 p-2 cursor-pointer bg-gray-100 rounded-full"
+          className="absolute left-5 p-2 cursor-pointer bg-gray-100 rounded-full"
         >
           <ArrowLeftIcon size={20} color="black" />
         </TouchableOpacity>
-      </View>
+      {/* </View> */}
 
+      {/* Form */}
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={name => changeName(name)}
+      />
+      <Text className="ml-4">Name: {name}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Country"
+        value={country}
+        onChangeText={country => changeName(country)}
+      />
+      <Text className="ml-4">Country: {country}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Image"
+        value={img}
+        onChangeText={img => changeImg(img)}
+      />
+      <Text className="ml-4">Image: {img}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Summary"
+        value={summary}
+        onChangeText={summary => changeSummary(summary)}
+      />
+      <Text className="ml-4">Summary: {summary}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Start Date"
+        value={startDate}
+        onChangeText={startDate => changeStartDate(startDate)}
+      />
+      <Text className="ml-4">Start Date: {startDate}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="End date"
+        value={endDate}
+        onChangeText={endDate => changeEndDate(endDate)}
+      />
+      <Text className="ml-4">End Date: {endDate}</Text>
+
+      <Button
+        onPress={onSubmit}
+        title="Submit"
+        className="bg-orange text-orange-500"
+        accessibilityLabel="Learn more about this purple button"
+      />
 
     </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    // marginTop: 80,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
